75d6dfe18bf021d77c934947a4c9cc48692fb77c
Cassey R. <rigodon.cv@obmontessori.edu.ph>
2026-04-21 07:38:18 +0800

    Car Counter

---
+++ main.js
@@ -0,0 +1,10 @@
+function countCars() {
+    let carCounter = 18;
+    const maxCars = 25;
+
+    while (carCounter < maxCars) {
+        window.alert("Car number " + carCounter + " is entering the garage.");
+        carCounter += 1;
+    }   
+
+}
